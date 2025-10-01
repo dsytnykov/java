9f13091307f5adb0152a9cb965b815e8bbfe677c
dsytnykov <dmitriy.sytnykov@gmail.com>
2025-10-01 19:15:36 +0200

    Leetcode task 104

---
+++ src/main/java/org/example/leetcode/tree/MaximumDepthOfBinaryTree_104.java
@@ -0,0 +1,30 @@
+package org.example.leetcode.tree;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaximumDepthOfBinaryTree_104 {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+
+        return Math.max(left, right);
+    }
+}
